b72ec43e41d11ab473e994b0442bf870e208c59d
etim <etimpaul22@gmail.com>
2023-09-27 07:07:41 +0100

    added useContracts hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { ethers } from "ethers";
 import CreateCampaign from "./component/CreateCampaign";
 import Header from "./component/Header";
+import useCampaign from "./hooks/useCampaigns";
+import { formatDate, shortenAccount } from "./utils";
 
 function App() {
+    const campaignCount = useCampaign()
     return (
-        <div className="App">
+        <div className="">
             <Header />
-            <main className="mt-10">
+
+            <main className="pt-10 App bg-gray-500 min-h-screen">
                 <CreateCampaign />
+                {campaignCount.length === 0 &&
+                    <span className="font-bold text-center block w-full text-3xl text-white mt-3">No Campaigns Yet</span>}
+                <div className="flex flex-wrap justify-center gap-10 px-20 py-10 w-ful">
+                    {!!campaignCount && campaignCount.map((campaign, index) =>
+                    (
+                        <div key={index} className="bg-white w-full sm:max-w-sm shadow-md rounded-xl overflow-hidden py-8">
+                            <div className="px-6 py-4">
+                                <h2 className="text-xl font-semibold text-gray-800">{campaign.title} - {shortenAccount(campaign?.owner)}</h2>
+                                <p className="mt-2 font-bold text-gray-600">Duration - {formatDate(ethers.formatUnits(campaign?.durationTime, 0))}</p>
+                                <p className="mt-2 font-bold text-gray-600">Balance - {ethers.formatUnits(campaign?.fundingBalance, 0)}ETH</p>
+                            </div>
+                            <div className="px-6 pt-4 pb-2">
+                                <span className="inline-block bg-blue-200 text-blue-800 text-sm px-4 py-2 rounded-full">{ethers.formatUnits(campaign.fundingGoal, 0)}ETH</span>
+                                <span className={`${campaign.isActive ? "bg-green-200 text-green-800" : "bg-gray-400 text-white"} inline-block text-sm px-4 py-2 rounded-full ml-2`}>{campaign.isActive ? "Active" : "Inactive"}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </main>
+
         </div>
     );
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -8,4 +8,32 @@ export const shortenAccount = (account) =>
     `${account.substring(0, 6)}...${account.substring(38)}`;
 
 export const getReadOnlyProvider = () =>
-    new ethers.JsonRpcProvider("https://goerli.base.org");
+    new ethers.JsonRpcProvider("https://sepolia.infura.io/v3/");
+
+export const crowdFundingAddress = "0x46f44F2D1af04D54ab5BCbEF9F4D0Df9baDc1B8C";
+
+export const formatDate = (time) => {
+
+    // Convert the timestamp to milliseconds by multiplying it by 1000
+    const date = new Date(time * 1000);
+
+    // Get the year, month, and day components
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // Months are zero-based, so add 1 to get the correct month
+    const day = date.getDate();
+
+    // Create an array of month names to map the numeric month to its name
+    const monthNames = [
+        'January', 'February', 'March', 'April',
+        'May', 'June', 'July', 'August',
+        'September', 'October', 'November', 'December'
+    ];
+
+    // Get the month name using the month value as an index in the monthNames array
+    const monthName = monthNames[month - 1];
+
+    const formattedDate = `${monthName} ${day}, ${year}`
+
+    return formattedDate;
+
+}
